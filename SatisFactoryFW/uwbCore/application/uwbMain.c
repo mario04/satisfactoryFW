cd25c50098b7949b09f3be7c9751d9d7f6f23b93
Mario Ruiz <marioalexander04@gmail.com>
2016-08-06 12:29:20 +0200

    Testing anchor case

--- SatisFactoryFW/uwbCore/application/uwbMain.c
+++ SatisFactoryFW/uwbCore/application/uwbMain.c
@@ -50,9 +50,9 @@
 #define BUTTON_0			FALSE
 
 #define TA_SW1_3			FALSE
-#define TA_SW1_4			FALSE		/* FALSE: Tag - TRUE: Anchor */
+#define TA_SW1_4			TRUE		/* FALSE: Tag - TRUE: Anchor */
 #define TA_SW1_5			FALSE
-#define TA_SW1_6			FALSE
+#define TA_SW1_6			TRUE
 #define TA_SW1_7			TRUE
 #define TA_SW1_8			FALSE
 
